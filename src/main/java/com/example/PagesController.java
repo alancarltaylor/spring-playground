c142eaa47421a974f437b78090600fbeca9203ce
Alan Taylor <alancarltaylor@gmail.com>
2017-02-18 12:31:52 -0700

    initial commit

---
+++ src/main/java/com/example/PagesController.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class PagesController{
+  @GetMapping("/hello")
+  public String hello(){
+    return "Hello World";
+  }
+}
